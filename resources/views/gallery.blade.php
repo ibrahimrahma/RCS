5c80f2d87ae8a7eda1270fcd49e38e2b44cc3ad4
ibrahimrahma <ibrahimrahma9876@gmail.com>
2023-09-28 00:49:23 -0700

    rahmas project

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,1 +1,3 @@
 <h1>gallery</h1>
+<img src="food.jpg" alt="Image Description" class="img-fluid">
+<p> 4500</p>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,46 +6,171 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
+  
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-body-primary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Eerah's eartry</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Name</title>
+    <!--   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Add your custom CSS if needed -->
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+<body>
+
+<!-- Navigation Bar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-danger-subtle">
+    <a class="navbar-brand" href="eerah's c">Eerahs cuisine</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./contact us">contact us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Reservations</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/home">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/bookings">bookings</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/gallery">gallery</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/prices">prices</a>
+            </li>
+            <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             dishes
           </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">intercontinental dishes</a></li>
             <li><a class="dropdown-item" href="#">local dishe</a></li>
             <li><a class="dropdown-item" href="#">smothies</a></li>
-          
+           
         </ul>
-        </li>
-      </ul>
     </div>
-  </div>
 </nav>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-md ">
-    <a class="navbar-brand text-center " href="#">Eerah's eatery</a>
+<!-- Hero Section -->
+<header class="hero bg-danger-subtle text-danger text-center py-5">
+    <div class="container" sans-serif;> 
+    
+
+      
+    </div>
+    </div>
+     <div class="container">
+     <img src="food.jpg" alt="Image Description" class="img-fluid">
+    <h1 class="display-4">Welcome to Eerahs cuisine</h1> 
+        <p class="lead">Flavors for royalty.</p>
+        <a href="#" class="btn btn-light btn-lg">View Menu</a>
+        
   </div>
-</nav>
+  
+</div>      
+    </div>
+</header>
+
+<!-- About Us Section -->
+<section class="about-us py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>About Us</h2>
+                <p>We, Eerahs cuisine at second avenue in gwarinpa, Abuja. are one of the best
+                cuisines in abuja. We pride ourselves in providing high quality food and also
+                serving amazing food. Eerahs imports and distributes fresh fruits and vegetables
+                across Nigeria, we also serve Salads, Smoothies, Parfaits, Greek Yogurt, Burgers<
+                Pizza, Sandwiches, Lunch Specials, BBQ and more across our restaurants.
+
+ 
+                </p>
+            </div>
+            <div class="col-lg-6">
+            </div>
+        </div>
+    </div>
+</section>
+<img src="food.jpg" class="object-fit-contain border rounded" alt="...">
+<img src="ruru.jfif" class="object-fit-cover border rounded" alt="...">
+<img src="rar.jfif" class="object-fit-fill border rounded" alt="...">
+<img src="ww.jpeg" class="object-fit-scale border rounded" alt="...">
+<img src="foodd.jpg" class="object-fit-none border rounded" alt="...">
+
+<!-- Menu Section -->
+<section class="menu py-5">
+    <div class="container">
+        <h2>Our Menu</h2>
+        <!-- Add your menu items here -->
+        <div class="row">
+            <div class="col-md-6">
+                <div class="menu-item">
+                    <h3>Menu Item 1</h3>
+                    <p>Description of the menu item. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <span class="price">$12.99</span>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="menu-item">
+                    <h3>Menu </h3>
+                    <p>Description of the menu item. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <span class="price">200</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="card" style="width: 18rem;">
+  <img src="chicken.jfif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">menu</h5>
+    <h3> suretos spagetti bologonesse</h3>
+    <p class="card-text">chunked chickens with delicious spagetti bologonesse</p>
+    <a href="order" class="btn btn-primary">order</a>
+  </div>
+</div>
+
+<!-- Contact Section -->
+<section class="contact py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>Contact Us</h2>
+                <p>If you have any questions or inquiries, feel free to contact us.</p>
+                <ul class="list-unstyled">
+                    <li><strong>Address:</strong> second avenue Gwarinpa, abuja</li>
+                    <li><strong>phone number:</strong><a href="Phone number">+234 813 616 4930</a></li>
+                    <li><strong>Email:</strong><a href="email@eerahscuisine.com">email@eerahscuisine.com</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-6">
+                <!-- 08136164930-->
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Footer -->
+<footer class="footer bg-danger text-white text-center py-3">
+    <div class="container">
+        <p>&copy; 2023 Eerahs cuisine. All Rights Reserved.</p>
+    </div>
+</footer>
+
+<!-- Add Bootstrap and jQuery Scripts -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
+
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
